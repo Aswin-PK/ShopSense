56c087f51ca6a229baa3c56ae163b24f53054f6d
Aswin-PK <aswinpk7737@gmail.com>
2023-06-17 21:02:56 +0530

    product edit and delete option for admin is added

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,5 +1,6 @@
 var db = require('../config/connection')
 var collection = require('../config/collections')
+var objectId = require('mongodb').ObjectId
 
 module.exports = {
 
@@ -13,10 +14,44 @@ module.exports = {
         })
     },
 
-    getProducts: ()=>{
+    getOneProduct: (productId)=>{
+        let id = new objectId(productId)
+        return new Promise(async (resolve, reject)=>{
+            let products = await db.get().collection(collection.productCollection).find({_id: id}).toArray()
+            resolve(products[0])
+        })
+    },
+
+    getAllProducts: ()=>{
         return new Promise(async (resolve, reject)=>{
             let products = await db.get().collection(collection.productCollection).find().toArray()
             resolve(products)
         })
-    }
+    },
+
+    deleteProduct: (productId)=>{
+        let id = new objectId(productId)
+        return new Promise((resolve, reject)=>{
+            db.get().collection(collection.productCollection).deleteOne({_id: id}).then(()=>{
+                resolve(true)
+            })
+        })
+    },
+
+    updateProduct: (productId, productDetail)=>{
+        let id = new objectId(productId)
+        return new Promise((resolve, reject)=>{
+            db.get().collection(collection.productCollection).updateOne({_id: id},{
+                $set: {
+                    name: productDetail.name,
+                    category: productDetail.category,
+                    price: productDetail.price
+                }
+            }).then(()=>{
+                resolve(true)
+            })
+        })    
+    },
+
+
 }
--- routes/admin.js
+++ routes/admin.js
@@ -4,27 +4,64 @@ var productHelpers = require('../helpers/product-helpers')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  productHelpers.getProducts().then((products)=>{
-    res.render('admin/view-products', {products, admin:true });
+  productHelpers.getAllProducts().then((products)=>{
+    res.render('admin/view-products', {products, admin:true});
   })
 });
 
 
 router.get('/add-products', (req, res)=>{
-  res.render('admin/add-products')
+  res.render('admin/add-products', {admin: true})
 });
 
 router.post('/add-products', (req, res)=>{
-  // console.log(req.body)
-  // console.log(req.files.image)
   productHelpers.addProduct(req.body, req.body.category, (objectId)=>{
     let image = req.files.image
     image.mv('./public/images/product-images/'+objectId+'.png', (err, done)=>{
-      if(!err) res.render('admin/add-products')
+      if(!err) res.redirect('/admin/add-products')
       else console.log(err)
     })
-    // res.render('admin/add-products')
   })
 });
+
+
+
+router.get('/delete-product', (req, res)=>{
+  productId = req.query.id
+  // console.log(req.query.id)
+  productHelpers.deleteProduct(productId).then((result)=>{
+    if(result) {
+      // console.log("object deleted")
+      res.redirect('/admin')
+    }
+  })
+})
+
+
+router.get('/edit-product', async (req, res)=>{
+  productId = req.query.id
+  // console.log(req.query.id)
+  let productDetail = await productHelpers.getOneProduct(productId)
+  console.log("admin.js ",productDetail,productId);
+  res.render('admin/edit-products', {productDetail, productId})
+})
+
+router.post('/edit-product', (req, res)=>{
+  productId = req.query.id
+  // console.log(req.body)
+  
+  productHelpers.updateProduct(productId, req.body).then((result)=>{
+    if(result) {
+      // console.log("object updated") 
+      if(req.files.image){
+        let image = req.files.image
+        image.mv('./public/images/product-images/'+productId+'.png', (err, done)=>{
+          if(!err) res.redirect('/admin')
+          else console.log(err)
+        }) 
+      }
+    }
+  })
+})
 
 module.exports = router;
